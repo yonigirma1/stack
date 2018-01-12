7655c9388ba1f51acfc1d7012354122432b18bda
Yonatan Girma <ygirma.exclusive@yahoo.com>
2018-01-12 13:03:13 -0600

    stack implementation using Python

---
+++ stack.py
@@ -0,0 +1,21 @@
+#stack implementation using Python OOP
+
+class Stack(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def sizeOf(self):
+        return len(self.items)
